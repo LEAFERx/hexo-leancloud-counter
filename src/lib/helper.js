2f72f352ce082382dabdd435c9a5c6f47c8a5b96
LEAFERx <leaferx@outlook.com>
2019-03-23 00:40:21 +0800

    fix typo

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -18,7 +18,7 @@ function scriptHelper() {
   const { config } = this;
   return this.fragment_cache(
     'leancloud-counter-script',
-    () => swig.renderFile(path.join(__dirname, './template/script.swig'),
+    () => swig.renderFile(path.join(__dirname, './template/mainScript.swig'),
       {
         app_id: config.leancloud_counter.app_id,
         app_key: config.leancloud_counter.app_key,
